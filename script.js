91b006c712a0b3c3c8b6f622b1dfab6ac0a4985d
kikalathiya <kikalathiya@algomau.ca>
2025-10-31 23:51:02 -0400

    Make the footer text change when button is clicked.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 function changeText() {
   document.querySelector("h1").innerText = "Button Clicked!";
+  document.querySelector("footer p").innerText = "You clicked the button at " + new Date().toLocaleTimeString();
 }
